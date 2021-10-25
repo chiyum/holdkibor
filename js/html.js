ca7948ea67dfa26a4eafdc68e71adfb4510ef90c
shengHong123 <91869779+shengHong123@users.noreply.github.com>
2021-10-25 18:01:26 +0800

    jstree完成80% 新增gotop

--- js/html.js
+++ js/html.js
@@ -8,4 +8,22 @@ $(document).ready(function () {
     $('.close_nav').on('click',function(){
         $('.mobile_nav').toggleClass('active');
     })
+    
+    $('.gotop').click(function(e){
+        $('html,body').animate({scrollTop:0},800)
+    })
 });
+
+let gotop = document.querySelector('.gotop');
+
+// console.log(document.body.offsetHeight)
+
+window.addEventListener('scroll',function(){
+
+    if(this.scrollY > 300){
+        gotop.classList.remove('active')
+    }else{
+        gotop.classList.add('active')
+    }
+
+})
